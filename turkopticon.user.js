9017cb19f9db1651f36367e080bc34b2fcdaef4a
Sim Singh <simsinght@gmail.com>
2020-05-04 11:34:04 -0700

    update to the most recent version on greasy fork and fix issues related to wrong name being chosen on the requesters page

--- turkopticon.user.js
+++ turkopticon.user.js
@@ -1,61 +1,67 @@
 // ==UserScript==
 // @name           turkopticon
-// @version        2014.08.20.1512
+// @version        2020.04.27.0
 // @description    Review requesters on Amazon Mechanical Turk
-// @author         Lilly Irani and Six Silberman
-// @homepage       http://turkopticon.ucsd.edu
+// @author         Lilly Irani, Six Silberman, anonymous contributors
+// @homepage       https://turkopticon.ucsd.edu
 // @include        http://*.mturk.com/*
 // @include        https://*.mturk.com/*
+// @namespace https://greasyfork.org/users/4606
 // ==/UserScript==
 
-var TURKOPTICON_BASE = "http://turkopticon.ucsd.edu/";
-var API_BASE = "http://turkopticon.ucsd.edu/api/";
+var TURKOPTICON_BASE = "https://turkopticon.ucsd.edu/";
+var API_BASE = "https://turkopticon.ucsd.edu/api/";
 var API_MULTI_ATTRS_URL = API_BASE + "multi-attrs.php?ids=";
 
 function getRequesterAnchorsAndIds(a) {
-    //a is a list of anchor DOM elements derived in the previous function
-        var rai = {};
-	var re = new RegExp(/requesterId/);
-	var rf = new RegExp(/contact/);
-        var isContactLink = new RegExp(/Contact/);
-	var isImgButton = new RegExp(/img/);
-	var requestersHere = false; 
+  //a is a list of anchor DOM elements derived in the previous function
+  var rai = {};
+  var requesterRegex = new RegExp(/requesterId=([0-9A-Z]+)|\/requesters\/([0-9A-Z]+)\/projects/);
+  var rf = new RegExp(/contact/);
+  var isContactLink = new RegExp(/Contact/);
+  var isImgButton = new RegExp(/img/);
+  var requestersHere = false;
 
-	for(var i = 0; i < a.length; i++) {
-		var href = a[i].getAttribute('href');
-		if (re.test(href) /*&& !rf.test(href)*/) {
-                    var innards = a[i].innerHTML;
-		    if (!isContactLink.test(innards) && !isImgButton.test(innards)) {
-			var id = a[i].href.split('requesterId=')[1].split('&')[0]
-			if (!rai.hasOwnProperty(id)) {
-				rai[id] = []; }
-			rai[id].push(a[i]);
-			requestersHere = true;
-		    } 
-		}
-	}
+  for (var i = 0; i < a.length; i++) {
+    var href = a[i].getAttribute('href');
+    var requesterIdMatch = requesterRegex.exec(href);
+    if ((requesterIdMatch) /*&& !rf.test(href)*/ ) {
+      var innards = a[i].innerHTML;
+      if (!isContactLink.test(innards) && !isImgButton.test(innards)) {
+        var id = requesterIdMatch[1] || requesterIdMatch[2];
+        if (!rai.hasOwnProperty(id)) {
+          rai[id] = [];
+        }
+        rai[id].push(a[i]);
+        requestersHere = true;
+      }
+    }
+  }
 
-	rai = (requestersHere)? rai : null; 
-	return rai; 
+  rai = (requestersHere) ? rai : null;
+  return rai;
 }
 
 function buildXhrUrl(rai) {
-	var url = API_MULTI_ATTRS_URL;
-	var ri = Object.keys(rai);
-	for(var i = 0; i < ri.length; i++) {
-		url += ri[i];
-		if (i < ri.length - 1) { url += ","; } }
-	return url; }
+  var url = API_MULTI_ATTRS_URL;
+  var ri = Object.keys(rai);
+  for(var i = 0; i < ri.length; i++) {
+    url += ri[i];
+    if (i < ri.length - 1) { url += ","; }
+  }
+  return url;
+}
 
 function makeXhrQuery(url) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, false);
-	xhr.send(null);
-	var resp = JSON.parse(xhr.response);
-	return resp; }
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.send(null);
+  var resp = JSON.parse(xhr.response);
+  return resp;
+}
 
 function ul(cl, inner) {
-	return "<ul class='" + cl + "'>" + inner + "</ul>"; }
+  return "<ul class='" + cl + "'>" + inner + "</ul>"; }
 
 function li(cl, inner) {
 	return "<li class='" + cl + "'>" + inner + "</li>"; }
@@ -108,7 +114,7 @@ function nrs(rid, nrevs) {
 	var str = "";
 	if (typeof nrevs === 'undefined') {
 		str = "<li>No reviews for this requester</li>"; }
-	else { str = "<li>Scores based on <a href='" + TURKOPTICON_BASE + rid + "'>" + nrevs + " reviews</a></li>"; }
+	else { str = "<li>Scores based on&nbsp;<a href='" + TURKOPTICON_BASE + rid + "'>" + nrevs + " reviews</a></li>"; }
 	return str; }
 
 function tos(tosflags) {
@@ -129,34 +135,42 @@ function dropDown(ro, rid) {
 	return dd; }
 
 function insertInlineCss() {
-	var css = "<style type='text/css'>\n";
-	   css += ".tob, .tom { list-style-type: none; padding-left: 0; }\n";
-	   css += ".tob { float: left; margin-right: 5px; }\n";
-	   css += ".tob > .tom { display: none; position: absolute; background-color: #ebe5ff; border: 1px solid #aaa; padding: 5px; }\n";
-	   css += ".tob:hover > .tom { display: block; }\n";
-	   css += ".tob > li { border: 1px solid #9db9d1; background-color: #ebe5ff; color: #00c; padding: 3px 3px 1px 3px; }\n";
-	   css += ".tob > li.toc { color: #f33; }\n";
-	   css += "@media screen and (-webkit-min-device-pixel-ratio:0) { \n .tob { margin-top: -5px; } \n}\n"
-	   css += ".attr { font-family: Monaco, Courier, monospace; color: #333; }\n";
-	   css += ".bar { font-size: 0.6em; }\n";
-	   css += ".unf { background-color: #ddd; }\n";
-	   css += ".red { background-color: #f00; }\n";
-	   css += ".yellow { background-color: #f90; }\n";
-	   css += ".green { background-color: #6c6; }\n";
-	   css += ".gray_link { margin-bottom: 15px; }\n";
-	   css += ".gray_link a { color: #666; }\n";
-	   css += "</style>";
-	var head = document.getElementsByTagName("head")[0];
-	head.innerHTML = css + head.innerHTML;
+    var head = document.getElementsByTagName("head")[0],
+        style = document.createElement('style'),
+        css = ".tob, .tom { list-style-type: none; padding-left: 0; }\n";
+    css += ".tob { float: left; margin-right: 5px; }\n";
+    css += ".tob > .tom { display: none; position: absolute; background-color: #ebe5ff; border: 1px solid #aaa; padding: 5px; z-index: 10;}\n";
+    css += ".tob:hover > .tom { display: block; }\n";
+    css += ".tob > li { border: 1px solid #9db9d1; background-color: #ebe5ff; color: #00c; padding: 3px 3px 1px 3px; }\n";
+    css += ".tob > li.toc { color: #f33; }\n";
+    css += "@media screen and (-webkit-min-device-pixel-ratio:0) { \n .tob { margin-top: -5px; } \n}\n";
+    css += ".attr { font-family: Monaco, Courier, monospace; color: #333; }\n";
+    css += ".bar { font-size: 0.6em; }\n";
+    css += ".unf { background-color: #ddd; }\n";
+    css += ".red { background-color: #f00; }\n";
+    css += ".yellow { background-color: #f90; }\n";
+    css += ".green { background-color: #6c6; }\n";
+    css += ".gray_link { margin-bottom: 15px; }\n";
+    css += ".gray_link a { color: #666; }\n";
+    style.textContent = css;
+    head.appendChild(style);
 }
+
 
 function getNames(rai, resp) {
 	for(var rid in rai) {
 		if (rai.hasOwnProperty(rid)) {
 			if (resp[rid] == "") {  // empty response, no data in Turkopticon DB for this ID
-				resp[rid] = JSON.parse('{"name": "' + rai[rid][0].innerHTML.split("</span>")[0].split(">")[1] + '"}'); 
+				resp[rid] = JSON.parse('{"name": "' + rai[rid][0].innerHTML.split("</span>")[0].split(">")[1] + '"}');
 			}
-			resp[rid].name = rai[rid][0].innerHTML.split("</span>")[0].split(">")[1]; } }  // overwrite name attribute of response object from page
+             // overwrite name attribute of response object from page:
+      if (rai[rid][0].innerText == "HITs") {
+        resp[rid].name = rai[rid][3].innerText;
+      } else {
+        resp[rid].name = rai[rid][0].innerHTML;
+      }
+    }
+  } // .split("</span>")[0].split(">")[1]; /* deprecated due to MTurk interface change */
 	return resp; }
 
 function insertDropDowns(rai, resp) {
